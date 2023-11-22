7534fede0e73a9f7e683342043c3262f718ea8de
Maria <m.leiliane.lobo@gmail.com>
2023-11-21 21:59:14 -0300

    assinatura dos metodos para manipular a objetos das classes de heranca

---
+++ conta/src/conta/repository/ContaRepository.java
@@ -0,0 +1,27 @@
+package conta.repository;
+
+import conta.model.Conta;
+
+public interface ContaRepository {
+
+	
+	// CRUD DA CONTA
+	public void procurarPorNumero(int numero);
+
+	public void listarTodas();
+
+	public void cadastrar(Conta conta);
+
+	public void atualizar(Conta conta);
+
+	public void deletar(int numero);
+	
+
+	// MÉTODOS BANCARIOS
+	public void sacar(int numero, float valor);
+
+	public void depositar(int numero, float valor);
+
+	public void transferir(int numeroOrigen, int numeroDestino, float valor);
+
+}
